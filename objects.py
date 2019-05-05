ae6ef999cbbc9bd819587cf06babee641c98688b
Casey Pastella <casey.pastella.15@cnu.edu>
2019-05-05 13:19:01 -0400

    update

--- objects.py
+++ objects.py
@@ -1,14 +1,35 @@
-class Dog():
-    def _init_(self, breed, gender, weight, color, name):
-        self.breed = breed
-        self.gender = gender
-        self.weight = weight
-        self.color = color
-        self.name = name
+class Account():
+    
+    def __init__(self, owner, balance):
         
-my_dog = Dog(breed= 'Lab', gender= 'male', weight = 100, color = 'brown', name = 'sammy')
+        self.owner = owner
+        self.balance = balance
+    
+    def deposit(self, num):
         
-          
+        print("{} was just added to your acount".format(num))
+        self.balance = self.balance + num
+    
+    def __str__(self):
+        return  "Account owner = {} \nYour Balance = {}".format(self.owner, self.balance)
+        
+    
+    def withdraw(self, num):
+        
+        if num <= self.balance:
+            self.balance = self.balance - num
+            print("Your new balance = {}".format(self.balance))
+        else:
+            print("Not enough funds")
+        
+        return self.balance
+        
+acc1 = Account('Casey', 1000)
 
+
+acc1.withdraw(500)
+acc1.withdraw(500)
+acc1.withdraw(1)
+print(acc1)
 
 
