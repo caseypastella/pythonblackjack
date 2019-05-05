bdc875bbf9a8bc9e1ac3eb764d6213ecb5aaa400
Casey Pastella <casey.pastella.15@cnu.edu>
2019-05-05 11:03:28 -0400

    update

---
+++ objects.py
@@ -0,0 +1,14 @@
+class Dog():
+    def _init_(self, breed, gender, weight, color, name):
+        self.breed = breed
+        self.gender = gender
+        self.weight = weight
+        self.color = color
+        self.name = name
+        
+my_dog = Dog(breed= 'Lab', gender= 'male', weight = 100, color = 'brown', name = 'sammy')
+        
+          
+
+
+
